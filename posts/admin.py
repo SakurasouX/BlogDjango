22b4d393b438d4c47f9f4d1cf9c3cc60b4a7a63c
Daniil Semenov <daniils2648@gmail.com>
2022-04-29 22:36:39 +0300

    some things to do with the administration panel

--- posts/admin.py
+++ posts/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Main information', {'fields': ['title', 'content']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    list_display = ('title', 'content', 'pub_date')
+    list_filter = ['pub_date']
+    # Adding search field for the admin panel
+    search_fields = ['title']
+
+
+admin.site.register(Post, PostAdmin)
