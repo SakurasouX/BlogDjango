9f8a5847d85cd06f569fcf5ff3b0af749a9e5585
Daniil Semenov <daniils2648@gmail.com>
2022-05-04 22:30:15 +0300

    added registration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Blog on Django
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -6,4 +8,28 @@ def login(request):
 
 
 def registration(request):
-    return render(request, 'accounts/registration.html')
+    """New user registration"""
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password')
+        password2 = request.POST.get('password_confirm')
+        if User.objects.filter(username=username):
+            return HttpResponse('A user with this username already exists')
+        if password1 == password2:
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password1,
+                first_name=first_name,
+                last_name=last_name
+            )
+            user.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponse('Пароли не совпадают')
+    else:
+        return render(request, 'accounts/registration.html')
+
