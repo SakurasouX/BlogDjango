3c1e2beb8fa1e512c1d708a3f6e2d2fa9c278fa8
Daniil Semenov <daniils2648@gmail.com>
2022-05-04 20:13:57 +0300

    update .gitignore and css

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('registration', views.registration, name='registration'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'accounts/login.html')
+
+
+def registration(request):
+    return render(request, 'accounts/registration.html')
