4c375dba926208509417eccf88579998d287fa29
Daniil Semenov <daniils2648@gmail.com>
2022-05-05 21:30:42 +0300

    added logout and password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.user_login, name='user_login'),
-    path('registration', views.registration, name='registration'),
+    path('registration/', views.registration, name='registration'),
+    path('personal_cabinet/', views.personal_cabinet, name='personal_cabinet'),
+    path('password_change/', views.password_change, name='password_change'),
+    path('logout/', views.user_logout, name='user_logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 
 def user_login(request):
-    """User login"""
+    """User authorization and return of user object"""
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect(reverse('posts:home'))
         else:
             return HttpResponse('Error')
     else:
         return render(request, 'accounts/login.html')
+
+
+def user_logout(request):
+    """user logout"""
+    logout(request)
+    return HttpResponseRedirect(reverse('posts:home'))
 
 
 def registration(request):
@@ -36,12 +43,29 @@ def registration(request):
                 email=email,
                 password=password1,
                 first_name=first_name,
-                last_name=last_name
+                last_name=last_name,
             )
             user.save()
             return HttpResponseRedirect('/')
         else:
-            return HttpResponse('Пароли не совпадают')
+            return HttpResponse('Passwords do not match')
     else:
         return render(request, 'accounts/registration.html')
 
+
+def personal_cabinet(request):
+    return render(request, 'accounts/personal_cabinet.html')
+
+
+def password_change(request):
+    """Change user password"""
+    if request.method == 'POST':
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 == password2:
+            user = request.user
+            user.set_password(password1)
+            user.save()
+            return HttpResponseRedirect('/')
+    else:
+        return render(request, 'accounts/password_change.html')
