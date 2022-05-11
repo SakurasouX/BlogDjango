d97a5a1823aae27bfcac6d23a1148f03ab48b91e
Daniil Semenov <daniils2648@gmail.com>
2022-05-11 22:32:10 +0300

    added new tests

--- posts/models.py
+++ posts/models.py
@@ -16,9 +16,9 @@ class Post(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', null=True)
     content = models.CharField(max_length=200)
     pub_date = models.DateTimeField(default=timezone.now)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', null=True)
 
     def __str__(self):
         return str(self.pub_date)
--- posts/tests/test_models.py
+++ posts/tests/test_models.py
@@ -1,20 +1,43 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from posts.models import Post
+from posts.models import Post, Comment
+
+
+def create_user():
+    user = User.objects.create_user(username='John', password='secret')
+    return user
+
+
+def create_post():
+    user = create_user()
+    post = Post.objects.create(
+        title='title',
+        content='content',
+        user=user
+    )
+    return post
 
 
 class PostModelTest(TestCase):
     def test_post_create(self):
         """Creating post with user and test his"""
-        user = User.objects.create_user('John')
-        post = Post.objects.create(
-            title='title',
-            content='content',
-            user=user
-        )
+        post = create_post()
         post.save()
-
         self.assertEqual('title', post.title)
         self.assertEqual('content', post.content)
-        self.assertEqual(user, post.user)
+        self.assertEqual(post.user, post.user)
+
+
+class CommentModelTest(TestCase):
+    def test_comment_create(self):
+        """Creating comments on some post and test his"""
+        post = create_post()
+        comment = Comment.objects.create(
+            user=post.user,
+            post=post,
+            content='Hello!'
+        )
+        self.assertEqual('Hello!', comment.content)
+        self.assertEqual(post.user, comment.user)
+        self.assertEqual(post, comment.post)
